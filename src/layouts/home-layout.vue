425dc9cf1657fc593d70ff323b0cefb2deb9ac56
bw007 <abdulaziz00791@gmail.com>
2024-02-09 02:13:34 +0500

    home container

--- src/layouts/home-layout.vue
+++ src/layouts/home-layout.vue
@@ -1,13 +1,14 @@
 <template>
-  <el-row>
-    <sidebar-part />
-    <router-view />
-  </el-row>
+  <el-container>
+    <el-aside width="auto">
+      <sidebar-part />
+    </el-aside>
+    <el-main>
+      <router-view />
+    </el-main>
+  </el-container>
 </template>
 
 <script setup>
 import sidebarPart from "@/components/sidebar-part.vue";
 </script>
-
-<style lang="">
-</style>
