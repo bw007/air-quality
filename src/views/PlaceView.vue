ed170c44bb734902e6a997d5b1a9d82ab94c35d5
bw007 <abdulaziz00791@gmail.com>
2024-02-09 02:13:14 +0500

    Place page

---
+++ src/views/PlaceView.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h1 style="margin-bottom: 20px;">Shaharlar</h1>
+    <el-input v-model="search" @change="handleSearch(search)" clearable placeholder="Country" />
+    <el-table :data="tableData" border style="width: 100%; margin-top: 40px;">
+      <el-table-column prop="date" label="Date" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="address" label="Address" />
+    </el-table>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let search = ref('')
+
+const handleSearch = (val) => {
+  console.log(val);
+}
+
+const tableData = ref([
+  {
+    date: new Date().getFullYear(),
+    name: "John Doe",
+    address: "Washington, USA"
+  }
+])
+
+</script>
+
+<style lang="">
+  
+</style>
