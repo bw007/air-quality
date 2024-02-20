34933c8acab0152de1883520271612493e2373e3
bw007 <abdulaziz00791@gmail.com>
2024-02-20 08:33:01 +0500

    Weather store

--- src/stores/data/weather.js
+++ src/stores/data/weather.js
@@ -1,14 +1,26 @@
-import { storeToRefs } from "pinia";
+import { defineStore } from "pinia";
 import { ref } from "vue";
 import { apiStore } from "../utils/api";
 import { urlData } from "../utils/env";
 
-export const weatherStore = storeToRefs("weatherStore", () => {
+export const weatherStore = defineStore("weatherStore", () => {
   const api = apiStore()
   const city = ref({})
 
   const getWeather = async (payload) => {
-    let res = await api.get()
+    let res = await api.get({ 
+      url: urlData.weatherUrl,
+      headers: {
+        'X-RapidAPI-Key': urlData.weatherKey,
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      },
+      params: { q: payload }
+    })
+
+    if (res?.status == 200) {
+      city.value = { ...res.data }
+    }
+    console.log(city.value);
   }
 
   return {
