d770f3f5118dcbbc8c886e54d63d7f6aa4d24a65
bw007 <abdulaziz00791@gmail.com>
2024-02-20 08:34:18 +0500

    Skeleton

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -6,33 +6,56 @@
       @change="handleSearch(search)"
       clearable
       placeholder="Misol: Jakarta"
+      style="margin-bottom: 20px;"
     />
+    
+    <el-skeleton animated>
+      <template #template>
+        <el-skeleton-item variant="circle" style="width: 70px; height: 70px;" />
+        <div style="padding: 5px; width: 300px;">
+          <el-skeleton-item variant="h1" style="width: 200px" />
+          <div style="margin-top: 10px;">
+            <el-skeleton-item variant="text"  />
+            <el-skeleton-item variant="text" />
+          </div>
+        </div>
+      </template>
+    </el-skeleton>
+
+
   </section>
 </template>
 
 <script setup>
-import { weatherStore } from "@/stores/data/weatherStore";
 
+
+import { weatherStore } from "@/stores/data/weather";
 import { storeToRefs } from "pinia";
 import { onMounted, ref } from "vue";
 // import { convertDate } from "@/stores/utils/func";
+import { placeStore } from "@/stores/data/place";
 
-const store = weatherStore();
 
+const place = placeStore()
+const store = weatherStore()
+
+const { loading, stations } = storeToRefs(place)
 const { city } = storeToRefs(store);
 
 let search = ref("");
 
 const handleSearch = async (val) => {
   if (!val) return
-  store.getWeather({route: "search", keyword: val.trim()});
+  store.getWeather({ route: "search", keyword: val.trim() });
 };
 
-  onMounted(() => {
-    if (!stations?.value.length) {
-      store.getStation({ route: "search", keyword: "Tashkent" });
-    }
-  })
+onMounted(() => {
+  if (!stations?.value.length) {
+    store.getWeather("Toshkent");
+  } else {
+    store.getWeather(stations?.value[0].station.geo.join())
+  }
+})
 
 </script>
 
