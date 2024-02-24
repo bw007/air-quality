df18e85c886d80c5647f118214e992d4c2a2ff89
bw007 <abdulaziz00791@gmail.com>
2024-02-24 22:35:05 +0500

    Bug fixed

--- src/stores/utils/env.js
+++ src/stores/utils/env.js
@@ -1,5 +1,5 @@
 export const urlData = {
-  waqiUrl: "http://api.waqi.info",
+  waqiUrl: "https://api.waqi.info",
   waqiToken: "c9b946f29ef0b19f6eb4b91474cbdf90cc01da60",
   apiKey: "AIzaSyAsqQO11ERrZ2jZlLUP5wMRawciLiK9-fo",
   mapsUrl: "https://maps.googleapis.com/maps/api/geocode/json",
